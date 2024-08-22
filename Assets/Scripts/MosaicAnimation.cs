860a9115e05afd2bd5b5e02726edfae8998e473f
HumbleBird <wltnsdid67@naver.com>
2024-08-22 19:56:26 +0900

    UI Shader Graph로 UI용 쉐이더 만들기

---
+++ Assets/Scripts/MosaicAnimation.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MosaicAnimation : MonoBehaviour, IMaterialModifier
+{
+    float _param;
+    bool _reveal;
+
+    public void Toggle() => _reveal = !_reveal;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _param += Time.deltaTime * (_reveal ? 1 : -1);
+        if (_param >= 0 && _param <= 1)
+            GetComponent<Image>().SetMaterialDirty();
+        _param = Mathf.Clamp01(_param);
+    }
+
+    public Material GetModifiedMaterial(Material baseMaterial)
+    {
+        var res = Mathf.Lerp(8, 512, Mathf.Pow(_param, 4));
+        baseMaterial.SetFloat("_Resolution", res);
+        return baseMaterial;
+    }
+}
